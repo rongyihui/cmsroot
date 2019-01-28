55a1df6a72be037c30b28e4d1eb97e0756296a0c
rongyihui <541884309@qq.com>
2019-01-28 09:26:48 +0800

    1月28日 没改

--- cms-core/src/test/java/com/rong/cms/dao/UserDaoTest.java
+++ cms-core/src/test/java/com/rong/cms/dao/UserDaoTest.java
@@ -1,7 +1,9 @@
 package com.rong.cms.dao;
 
 
+import com.rong.cms.model.Role;
 import com.rong.cms.model.User;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/beans.xml")
@@ -33,6 +36,11 @@ public class UserDaoTest extends BaseDaoTest{
                 ,"5418@qq.com","12345678901","0"
                 ,new Date(),new Date());
         userDao.add(user);
+        Assert.assertEquals("用户添加失败",user.getNickname(),userDao.load(user.getId()).getNickname());
     }
-
+    @Test
+    public void listUserRoles(){
+        List<Role> roles = userDao.listUserRoles(2);
+        Assert.assertEquals("list指定用户角色列表查询失败",3,roles.size());
+    }
 }
